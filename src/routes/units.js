5f9d9f1d7e20fe9b5de57da6b2b066bcbcce6fb4
tom <lim1017@hotmail.com>
2020-08-12 23:45:49 -0400

    can add and edit units to a company

--- src/routes/units.js
+++ src/routes/units.js
@@ -18,8 +18,6 @@ module.exports = db => {
 
   router.post("/unit/create", (request, response)=>{
 
-    console.log(request.body)
-
     const { unit, rent, sqft, bedroom, tmi, notes, propertyId  } = request.body
     let issues = {}
 
@@ -38,6 +36,38 @@ module.exports = db => {
     });
 
   })
+
+
+  router.patch("/unit/edit/:unitid", (request, response)=>{
+
+    const unitid = request.params.unitid
+    console.log(unitid)
+    console.log(request.body)
+
+    const { unit, rent, sqft, bedroom, tmi, notes, unitId  } = request.body
+
+    db.query(
+    `
+      UPDATE units
+      SET
+      unit = $1,
+      sqft = $2,
+      rent = $3,
+      bedroom = $4,
+      tmi = $5,
+      notes = $6
+
+      WHERE unit_id = $7
+ 
+    `,
+    [unit, sqft, rent, bedroom, tmi, notes, unitId]
+    ).then((res) => {
+      console.log(res, 'res!!!!!!!!!!!!!!!!!!')
+      response.json(res.rows.results);
+    });
+
+  })
+
   
 
   router.get("/units/:propertyId", (request, response) => {
@@ -57,6 +87,25 @@ module.exports = db => {
     });
   });
   
+  router.get("/unit/:unitId", (request, response) => {
+    console.log('getting 1 unit')
+    unitId = request.params.unitId;
+
+    db.query(
+      `
+      SELECT *
+      FROM units
+      WHERE unit_id = $1
+    `,
+      [unitId]
+    ).then(({ rows: results }) => {
+      // console.log(user);
+      response.json(results);
+    });
+  });
+
+  
+
 
 
   return router;
